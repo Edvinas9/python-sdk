c0f03eba223e2ebea4c77e0ea686b27d0b6f72eb
Edvinas Ilcenko <edvinas@ilcenko.com>
2021-09-22 16:33:32 +0300

    CTD-16 API PUT request and update_payment_intent to use PUT

--- livestyled/schemas/payment.py
+++ livestyled/schemas/payment.py
@@ -42,7 +42,7 @@ class PaymentSourceSchema(Schema):
 
     id = fields.Int(missing=None)
     status = fields.String()
-    payment_customer = RelatedResourceLinkField(schema=PaymentCustomerSchema, data_key='paymentCustomer', microservice_aware=True)
+    payment_customer = RelatedResourceLinkField(schema=PaymentCustomerSchema, data_key='paymentCustomer', microservice_aware=True, missing=None)
     token_provider = fields.String(data_key='tokenProvider')
     external_id = fields.String(data_key='externalId')
     psp = fields.String()
@@ -62,8 +62,8 @@ class PaymentIntentSchema(Schema):
 
     id = fields.Int(missing=None)
     external_id = fields.String(data_key='externalId', missing=None)
-    payment_source = RelatedResourceLinkField(schema=PaymentSourceSchema, data_key='paymentSource', microservice_aware=True)
-    payment_customer = RelatedResourceLinkField(schema=PaymentCustomerSchema, data_key='paymentCustomer', microservice_aware=True)
+    payment_source = RelatedResourceLinkField(schema=PaymentSourceSchema, data_key='paymentSource', microservice_aware=True, missing=None)
+    payment_customer = RelatedResourceLinkField(schema=PaymentCustomerSchema, data_key='paymentCustomer', microservice_aware=True, missing=None)
     status = fields.String()
     amount = fields.Int()
     currency = fields.String()
